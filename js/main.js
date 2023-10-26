d64a69c8e37c7439ec53418fda972b9bbb7145d3
Emanuele-Guerra <emaguerra1993@hotmail.it>
2023-10-26 16:24:20 +0200

    end

--- js/main.js
+++ js/main.js
@@ -13,17 +13,17 @@ let prezzoTotale = (km * prezzoPerKm);
 if (eta > 65) {
     (prezzoTotale *= 0.6)
     {
-        document.getElementById("biglietto").innerHTML = "il prezzo del tuo biglietto con sconto del 40% è di: " + prezzoTotale.toFixed(2) + " \u20AC";
+        document.getElementById("biglietto").innerHTML = `il prezzo del tuo biglietto con sconto del 40% è di: ${prezzoTotale.toFixed(2)}` + " \u20AC";
     }
 }
 
 else if (eta < 18) {
     (prezzoTotale *= 0.8)
     {
-        document.getElementById("biglietto").innerHTML = "il prezzo del tuo biglietto con sconto del 20% è di: " + prezzoTotale.toFixed(2) + " \u20AC";
+        document.getElementById("biglietto").innerHTML = `il prezzo del tuo biglietto con sconto del 20% è di: ${prezzoTotale.toFixed(2)}` + " \u20AC";
     }
 }
 
 else (prezzoPerKm * km) 
-    document.getElementById("biglietto").innerHTML = "il prezzo del tuo biglietto è di: " + prezzoTotale.toFixed(2) + " \u20AC";
+    document.getElementById("biglietto").innerHTML = `il prezzo del tuo biglietto è di: ${prezzoTotale.toFixed(2)}` + " \u20AC";
 
